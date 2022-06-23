4e15789f85bda803915fdb775f7d6453ca4d2db8
Joe Curtis <joecurtis9999@gmail.com>
2022-06-23 17:57:15 +0100

    projects section

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,15 +1,15 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
+    title: 'webvisuals',
+    description: "Using HTML, CSS and Javascript, I built a one-page business website from scratch with a clean, modern user interface. The cards change colour when a user hovers over the card and a the page also contains a carousel.",
+      image: '/images/webvish2.jpg',
       tags: ['Mongo', 'Express', 'React', 'Node'],
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 0,
   },
   {
-    title: 'E-Commerce',
+    title: 'MovieWatchList',
     description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
     image: '/images/2.png',
     tags: ['React', 'JavaScript'],
@@ -18,7 +18,7 @@ export const projects = [
     id: 1,
   },
   {
-    title: 'WebRTC App',
+    title: 'CryptoGuide',
     description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
       image: '/images/3.jpg',
       tags: ['React', 'WebRTC'],
@@ -27,7 +27,7 @@ export const projects = [
     id: 2,
   },
   {
-    title: 'Unichat',
+    title: 'Eventech',
     description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
     image: '/images/4.jpg',
     tags: ['React', 'ChatEngine', 'Firebase'],
