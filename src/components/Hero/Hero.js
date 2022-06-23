8b5e46ab8e0c9ec3cd8dd09a338c01ec2f831491
Joe Curtis <joecurtis9999@gmail.com>
2022-06-23 17:07:22 +0100

    added hero title description and discover more button which takes to github

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        my personal portfolio
+      </SectionTitle>
+      <SectionText>
+        On this site you will find both personal and group projects that
+        I have been working on over the past year
+      </SectionText>
+      <Button onClick={() => window.location = 'https://github.com/JosephCurtis1999'}>Discover More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
